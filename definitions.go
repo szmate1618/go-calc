7b7e44b62ce83c41753bf68abb3f229a38789f63
Máté Szabó <szmate1618@gmail.com>
2023-03-12 19:34:02 +0100

    nit

--- definitions.go
+++ definitions.go
@@ -3,7 +3,7 @@ package main
 const prompt string = "> "
 
 const greeting string = "Are you ready for some MATH?"
-const farewell string = "Thank you for using this incredibly sophisticated piece of software"
+const farewell string = "Thank you for using this incredibly sophisticated piece of software."
 
 const colorGreen string = "\033[32m"
 const colorReset string = "\033[0m"
